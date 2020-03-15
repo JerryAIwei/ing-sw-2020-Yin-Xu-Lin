c33c3c4c56cd2031401115366742f2425682279e
xsxun <xsxuno@outlook.com>
2020-03-15 17:44:42 +0100

    init IDEA project

---
+++ src/main/java/it/polimi/ingsw/App.java
@@ -0,0 +1,10 @@
+package it.polimi.ingsw;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Polimi Software Engineering Project Santorini." );
+    }
+}
