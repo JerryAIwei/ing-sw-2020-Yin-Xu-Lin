fe8af5b1f2806c16c0030451c640ea96b28a9a5d
Lin-Xin1118 <10734971@polimi.it>
2020-04-05 22:58:42 +0200

    update Apollo and Minotaur godpower

--- src/main/java/it/polimi/ingsw/xyl/model/cosplayer/Minotaur.java
+++ src/main/java/it/polimi/ingsw/xyl/model/cosplayer/Minotaur.java
@@ -1,11 +1,16 @@
 package it.polimi.ingsw.xyl.model.cosplayer;
 
-import it.polimi.ingsw.xyl.model.Cosplayer;
-import it.polimi.ingsw.xyl.model.Direction;
-import it.polimi.ingsw.xyl.model.IslandBoard;
-import it.polimi.ingsw.xyl.model.Player;
+import it.polimi.ingsw.xyl.model.*;
+
+import java.util.EnumSet;
+import java.util.Vector;
 
 import static it.polimi.ingsw.xyl.model.GodPower.MINOTAUR;
+
+/**
+ * @author Lin Xin
+ */
+
 
 public class Minotaur extends Cosplayer {
 
@@ -29,13 +34,53 @@ public class Minotaur extends Cosplayer {
         int targetOccupiedBy = currentIslandBoard.getSpaces()
                 [originalPositionX + direction.toMarginalPosition()[0]]
                 [originalPositionY + direction.toMarginalPosition()[1]].isOccupiedBy();
-        int backwardsPositionX = originalPositionX + direction.toMarginalPosition()[0] * 2;
-        int backwardsPositionY = originalPositionY + direction.toMarginalPosition()[1] * 2;
-        int backwardsOccupiedBy = currentIslandBoard.getSpaces()
-                [backwardsPositionX]
-                [backwardsPositionY].isOccupiedBy();
-        if(targetOccupiedBy != -1 && backwardsOccupiedBy == -1){
+        int backwardsPositionX = originalPositionX + 2 * direction.toMarginalPosition()[0];
+        int backwardsPositionY = originalPositionY + 2 * direction.toMarginalPosition()[1];
+        int backwardsOccupiedBy = currentIslandBoard.getSpaces()[backwardsPositionX][backwardsPositionY].isOccupiedBy();
+        boolean backwardsNoDome = currentIslandBoard.getSpaces()[backwardsPositionX][backwardsPositionY].getLevel() != Level.DOME;
+        EnumSet<Direction> all = EnumSet.allOf(Direction.class);
+        Vector<Direction> minotaurAvailableMoves = new Vector<>(all);
 
+        if (originalPositionX == 1 || originalPositionX == 0) {
+            minotaurAvailableMoves.remove(Direction.LEFT);
+            minotaurAvailableMoves.remove(Direction.UPLEFT);
+            minotaurAvailableMoves.remove(Direction.DOWNLEFT);
         }
+        if (originalPositionX == 3 || originalPositionX == 4) {
+            minotaurAvailableMoves.remove(Direction.RIGHT);
+            minotaurAvailableMoves.remove(Direction.UPRIGHT);
+            minotaurAvailableMoves.remove(Direction.DOWNRIGHT);
+        }
+        if (originalPositionX == 1 || originalPositionY == 0) {
+            minotaurAvailableMoves.remove(Direction.DOWN);
+            minotaurAvailableMoves.remove(Direction.DOWNLEFT);
+            minotaurAvailableMoves.remove(Direction.DOWNRIGHT);
+        }
+        if (originalPositionX == 3 || originalPositionY == 4) {
+            minotaurAvailableMoves.remove(Direction.UP);
+            minotaurAvailableMoves.remove(Direction.UPLEFT);
+            minotaurAvailableMoves.remove(Direction.UPRIGHT);
+        }
+
+        if(targetOccupiedBy != -1 && backwardsOccupiedBy == -1 && backwardsNoDome && minotaurAvailableMoves.contains(direction)) {
+            int opponentWorkerId = targetOccupiedBy % 10;
+            int opponentPlayerId = targetOccupiedBy / 10;
+            Space opponentCurrentSpace = currentIslandBoard.getSpaces()
+                    [originalPositionX + direction.toMarginalPosition()[0]]
+                    [originalPositionY + direction.toMarginalPosition()[1]];
+            Vector<Direction> availableMoves = getAvailableMoves(worker);
+
+            super.move(worker, direction);
+            availableMoves.addElement(direction);
+            this.getPlayer().getCurrentGameBoard().getPlayers().get(opponentPlayerId).getWorkers()
+                    [opponentWorkerId].setFromLevel(opponentCurrentSpace.getLevel().toInt());
+            opponentCurrentSpace.setOccupiedBy(-1);
+            this.getPlayer().getCurrentGameBoard().getPlayers().get(opponentPlayerId).getWorkers()
+                    [opponentWorkerId].setPosition(backwardsPositionX, backwardsPositionY);
+            currentIslandBoard.getSpaces()[backwardsPositionX][backwardsPositionY].setOccupiedBy(opponentPlayerId * 10 + worker);
+            checkWin();
+            System.out.println("Minotaur's move");
+        }
+        super.move(worker, direction);
     }
 }
